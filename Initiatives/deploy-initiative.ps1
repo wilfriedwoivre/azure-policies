c7b4818d06f84186e5976f5e25ac3c7d92124354
Wilfried Woivré <wilfried.woivre@gmail.com>
2024-04-11 15:25:48 +0200

    Add metadata for test

--- Initiatives/deploy-initiative.ps1
+++ Initiatives/deploy-initiative.ps1
@@ -9,11 +9,29 @@ param (
     
 )
 
-[Array]$policies = Get-AzPolicyDefinition -Custom | Where-Object { $_.properties.Metadata.category -match 'AzSecure' }
+Get-ChildItem -Filter "*.json" -Recurse -Path "$PSScriptRoot\..\Policies" | ForEach-Object {
+    Write-Host "Deploying policy definition $($_.FullName)"
+    $policy = Get-Content -Path $_.FullName | ConvertFrom-Json -Depth 10
+
+    New-AzPolicyDefinition -Name $policy.properties.displayName `
+                           -DisplayName $policy.properties.displayName `
+                           -Description $policy.properties.description `
+                           -Policy $($policy.properties.policyRule | convertto-json -Depth 10) `
+                           -Metadata $($policy.properties.metadata | convertto-json -Depth 10) `
+                           -Parameter $($policy.properties.parameters | convertto-json -Depth 10) `
+                           -ManagementGroupName $managementGroupId `
+                           -Mode $policy.properties.mode | Out-Null
+                           
+
+
+}
+
+
+$policies = Get-AzPolicyDefinition -Custom | Where-Object { $_.properties.Metadata.category -match 'AzSecure' }
 
 $groupNames = $policies.properties.Metadata.category | %{ ($_ -split '-')[1] } | Select-Object -Unique -Property @{ label='name'; expression={$_}} | convertto-json
 
-[Array]$policyDefinitions = @()
+$policyDefinitions = @()
 $initiativeParameters = New-Object -type psobject
 
 foreach ($policy in $policies) {
@@ -31,7 +49,7 @@ foreach ($policy in $policies) {
     $policyDefinitions += @{ "policyDefinitionId"= $policy.PolicyDefinitionId; "groupNames"= @( $policy.Properties.Metadata.category | %{ ($_ -split '-')[1] } ); "parameters" = $policyParameters}
 }
 
-$metadata = @{ "category"= "AzSecure"; "version"="1.0.0" }
+$metadata = @{ "category"= "AzSecure"; "version"="1.0.0"; "ASC"="true" }
 
 
 New-AzPolicySetDefinition -Name 'AzSecure' -DisplayName 'AzSecure' -Description 'Azure Policies from https://github.com/wilfriedwoivre/azure-policies' -Metadata ($metadata | ConvertTo-Json) -PolicyDefinition ($policyDefinitions | ConvertTo-Json -Depth 10) -Parameter ($initiativeParameters | ConvertTo-Json -Depth 5) -GroupDefinition $groupNames -ManagementGroupName $managementGroupId
